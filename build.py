35ef7a9fef095bd085e7a3d94066445ba0664a7a
nitikagoel01 <nitikagoel.1693@gmail.com>
2017-06-19 16:51:24 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,12 @@
-def solution(dic1, dic2):
-    """Enter Code Here"""
+
+def solution(d1, d2):
+    d3 ={}
+    for x,y in d1.items():
+        if x in d2.keys():
+            d3[x]=d1[x]+d2[x]
+        else:
+            d3[x]=d1[x]
+    for x,y in d2.items():
+        if x not in d1.keys():
+            d3[x]=d2[x]
+    return (d3)
